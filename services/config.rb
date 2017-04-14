f053ac10469cb5c6bffd5ad1c9360ec890123444
Paul Allen <pauldallen@gmail.com>
2017-04-14 11:40:56 -0700

    adding in the jsrunner

--- services/config.rb
+++ services/config.rb
@@ -42,15 +42,63 @@ coreo_uni_util_jsrunner "usage" do
   action :run
   json_input 'COMPOSITE::coreo_aws_rule_runner.usage.report'
   function <<-EOH
+    var new_report = {};
 
-    callback(json_input);
+    for (region in json_input) {
+        for (inspector_rule_arn in json_input[region]) {
 
+            var violations = json_input[region][inspector_rule_arn]['violations'];
+            var violator_info = json_input[region][inspector_rule_arn]['violator_info'];
+            var tags = json_input[region][inspector_rule_arn]['tags'];
+            var violator_id = violator_info['asset_attributes']['agent_id'];
+
+            var ruleid = violator_info['id'];
+            var service = violator_info['service'];
+            var display_name = violator_info['attributes'].find(o => o.key === 'BENCHMARK_RULE_ID')['value'];
+            var description = violator_info['description'];
+            var suggested_action = violator_info['recommendation'];
+            var level = violator_info['severity'];
+
+            var meta_cis_id = display_name.split()[0];
+            var meta_cis_level = violator_info['attributes'].find(o => o.key === 'CIS_BENCHMARK_PROFILE')['value'];
+
+            for (cc_rule_name in violations) {
+
+                var result_info = violations[cc_rule_name]['result_info'];
+                var category = violations[cc_rule_name]['category'];
+                var include_violations_in_count = violations[cc_rule_name]['include_violations_in_count'];
+
+                if (!new_report[region]) { new_report[region] = {} };
+                if (!new_report[region][violator_id]) { new_report[region][violator_id] = {} };
+                if (!new_report[region][violator_id]['violator_info']) { new_report[region][violator_id]['violator_info'] = {} };
+                if (!new_report[region][violator_id]['violations']) { new_report[region][violator_id]['violations'] = {} };
+                if (!new_report[region][violator_id]['violations'][ruleid]) { new_report[region][violator_id]['violations'][ruleid] = {} };
+
+                new_report[region][violator_id]['violator_info']['id'] = violator_id;
+                new_report[region][violator_id]['violations'][ruleid].service = service;
+                new_report[region][violator_id]['violations'][ruleid].display_name = display_name;
+                new_report[region][violator_id]['violations'][ruleid].description = description;
+                new_report[region][violator_id]['violations'][ruleid].category = category;
+                new_report[region][violator_id]['violations'][ruleid].suggested_action = suggested_action;
+                new_report[region][violator_id]['violations'][ruleid].level = level;
+                new_report[region][violator_id]['violations'][ruleid].include_violations_in_count = include_violations_in_count;
+                new_report[region][violator_id]['violations'][ruleid].region = region;
+                new_report[region][violator_id]['violations'][ruleid].result_info = result_info;
+                new_report[region][violator_id]['violations'][ruleid].meta_cis_id = meta_cis_id;
+                new_report[region][violator_id]['violations'][ruleid].meta_cis_level = meta_cis_level;
+                new_report[region][violator_id]['violations'][ruleid].meta_always_show_card = true;
+
+            }
+        }
+    }
+
+    callback(new_report);
   EOH
 end
 
-# coreo_uni_util_variables "findings-var" do
-#   action :set
-#   variables([
-#                 {'COMPOSITE::coreo_aws_rule_runner.usage.report' => 'COMPOSITE::coreo_uni_util_jsrunner.usage.report'},
-#             ])
-# end
+coreo_uni_util_variables "findings-var" do
+  action :set
+  variables([
+                {'COMPOSITE::coreo_aws_rule_runner.usage.report' => 'COMPOSITE::coreo_uni_util_jsrunner.usage.report'},
+            ])
+end
